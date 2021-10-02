d926ddff2a189b344d6e59bc7e0ffaff7d55a3df
Josh Oldes <jcoldes@outlook.com>
2021-10-02 04:06:59 -0700

    Creating basic window class

---
+++ DirectXGame/DirectXGame/Window.cpp
@@ -0,0 +1,19 @@
+#include "Window.h"
+
+Window::Window()
+{
+}
+
+bool Window::init()
+{
+	return true;
+}
+
+bool Window::release()
+{
+	return true;
+}
+
+Window::~Window()
+{
+}
---
+++ DirectXGame/DirectXGame/Window.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class Window
+{
+public:
+	Window();
+	bool init();		// initialize window
+	bool release();		// destroy window
+	~Window();
+};
+
